2df544de1720892f6a5e2aaa5501688714826859
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-07-20 02:13:15 +0200

    init nuxt

---
+++ flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "nuxt";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShell = pkgs.mkShell {
+        packages = with pkgs; [
+            nodejs_22
+            pnpm
+        ];
+      };
+    });
+}
