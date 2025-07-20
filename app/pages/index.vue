c616a929fdb4e38bdfb4dd6fe0f2c3fc34b4fb19
suiluj482 <84525736+suiluj482@users.noreply.github.com>
2025-07-20 04:20:11 +0200

    .

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,7 +8,6 @@ const githubUrl = 'https://github.com/suiluj482'
   <div class="min-h-screen bg-white dark:bg-gray-900 flex flex-col items-center pt-20">
     <UCard class="p-8 text-center w-full max-w-md shadow-lg">
       <NuxtPicture
-        format="webp,avif"
         src="/images/js.png"
         alt="Logo"
         class="mx-auto mb-4 w-20 h-20 rounded-full shadow-md"
